5c35e36b875826ade21af31cbd6adeab10b5f34a
Omkumar25091210 <74484315+Om25091210@users.noreply.github.com>
2021-01-08 23:22:37 +0530

    COMMIT
    COMMIT

--- app/src/main/java/com/example/pushnotification/Constants.kt
+++ app/src/main/java/com/example/pushnotification/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object{
         const val  BASE_URL="https://fcm.googleapis.com"
-        const val SERVER_KEY="AAAA4NgDoIs:APA91bGFpKhsVmEG9xWmaCbtp6yiyw211kIVpo1gmFkHV54KleF7HseRB6q9yeSS2GoTgPZmXW42QvLhcEWVdxgtdNv1f2kbN6FZYHv_hvKlT-M_bQ-B11Hdfhum3GbOsq20G6J1SSTg"
+        const val SERVER_KEY="ENTER YOUR SERVER KEY FROM FIREBASE BY GOING TO : project overview-->project setting--> cloud messaging "
         const val CONTENT_TYPE="application/json"
     }
 }
